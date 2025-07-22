e8368deed77418bc44ee49e9afa4f27a83d84cec
Marcela Gama Le <marcela-dev@Marcelas-MacBook-Air.local>
2025-07-21 19:20:22 -0700

    <patent> 📱 Fixed Capacitor setup: added platform folders + TypeScript dependency

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marketingmother.motherboard',
+  appName: 'Motherboard',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
