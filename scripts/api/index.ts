d75dc8a22275eea437e3423806b1c788a33ac1c4
Marcela Gama Le <marcelagamale@gmail.com>
2025-10-06 15:21:58 -0700

    <0001fabe> Fix: synchronous import for reflections router to mount before listen

--- scripts/api/index.ts
+++ scripts/api/index.ts
@@ -1,12 +1,8 @@
-// <0001fabc> Express router for reflections API (CommonJS-safe loader)
+// <0001fabe> Express router for reflections API (sync import)
 import express from "express";
+import { reflectionsAllHandler } from "./reflections-all";
+import { reflectionsLatestHandler } from "./reflections-latest";
 
 export const reflectionsRouter = express.Router();
-
-(async () => {
-  const { reflectionsAllHandler } = await import("./reflections-all.js").catch(() => require("./reflections-all"));
-  const { reflectionsLatestHandler } = await import("./reflections-latest.js").catch(() => require("./reflections-latest"));
-
-  reflectionsRouter.get("/all", (req, res) => reflectionsAllHandler(req, res));
-  reflectionsRouter.get("/latest", (req, res) => reflectionsLatestHandler(req, res));
-})();
+reflectionsRouter.get("/all", reflectionsAllHandler);
+reflectionsRouter.get("/latest", reflectionsLatestHandler);
