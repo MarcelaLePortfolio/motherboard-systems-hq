e5047a96b21e97bb652114cc57dca6639b137066
Marcela Gama Le <marcelagamale@gmail.com>
2026-03-14 17:42:00 -0700

    Phase 65B: harden telemetry bootstrap loader idempotence

--- public/js/telemetry/phase65b_metric_bootstrap.js
+++ public/js/telemetry/phase65b_metric_bootstrap.js
@@ -7,30 +7,26 @@ NO BUNDLE MUTATION
 ISOLATED LOADER ONLY
 */
 
-(function(){
+(function () {
+  const SCRIPT_ID = "phase65b-running-tasks-metric";
 
-function loadScript(src){
+  function loadScript(src) {
+    if (document.getElementById(SCRIPT_ID)) return;
 
     const s = document.createElement("script");
-
+    s.id = SCRIPT_ID;
     s.src = src;
-
     s.defer = true;
-
     document.body.appendChild(s);
+  }
 
-}
-
-function init(){
-
+  function init() {
     loadScript("/js/telemetry/running_tasks_metric.js");
+  }
 
-}
-
-if(document.readyState === "loading"){
+  if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", init);
-}else{
+  } else {
     init();
-}
-
+  }
 })();
