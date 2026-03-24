a98c67514d50c4908bad0689ed1ce83d2a257bf0
Marcela Gama Le <marcelagamale@gmail.com>
2026-03-23 19:56:05 -0700

    phase122: export consumption registry constants

--- src/cognition/transport/consumptionRegistry/index.ts
+++ src/cognition/transport/consumptionRegistry/index.ts
@@ -1,3 +1,4 @@
+export * from "./dashboardConsumption.registry.constants";
 export * from "./dashboardConsumption.registry.types";
 export * from "./dashboardConsumption.registry";
 export * from "./dashboardConsumption.registry.invariants";
